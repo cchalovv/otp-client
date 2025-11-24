979cf3a8fda8f251fed56c9a0f8795442a2ccc44
chingizkhan <cchalovv@mail.ru>
2025-11-24 10:12:50 +0500

    Fix: added json tags

--- model/model.go
+++ model/model.go
@@ -2,12 +2,12 @@ package model
 
 type GenerateRequest struct {
 	// Data is some payload or identifier to uniquely generate OTP code
-	Data string
+	Data string `bson:"data"`
 }
 
 type VerifyRequest struct {
 	// Data is the same data used to generate code
-	Data string
+	Data string `json:"data"`
 	// Code is OTP code sent to phone/email
-	Code string
+	Code string `bson:"code"`
 }
